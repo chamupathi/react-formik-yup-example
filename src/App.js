e188d1e3da5cd61815436db2a218c108d13128c5
chamupathi <chamupathi@blimpit.com>
2018-08-19 14:02:53 +0530

    formik anf yup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Formik } from 'formik'
+import * as Yup from 'yup' 
 
 class App extends Component {
 
@@ -12,7 +13,7 @@ class App extends Component {
   handleChange = (event) => {
     this.setState(
       {
-        [event.target.name] : event.target.value
+        [event.target.name]: event.target.value
       }
     )
     this.validateFields()
@@ -25,31 +26,67 @@ class App extends Component {
     this.state.password.length < 5 ? errorMessage = "password should be more than 5 chars" : errorMessage = null
 
     this.setState({
-      passwordError:errorMessage
+      passwordError: errorMessage
     })
   }
-  
+
 
   onSigninClicked = () => {
     console.log("username is : " + this.state.userName + " password is : " + this.state.password)
   }
 
+  handleSubmit = (values) => {
+    console.log(values)
+  }
+
+  validationSchema  = Yup.object().shape({
+    userName: Yup.string()
+    .email("invalid email Address")
+    .min(9, "minimum length is 9 chars")
+    .required("user name is required"),
+
+    password: Yup.string()
+    .min(5, "min length is 5 chars")
+    .required("password is required")
+  })
+
   render() {
     return (
-      <div className="App">
-        <h1>Login Example</h1>
 
-        <label htmlFor="userName">User Name</label>
-        <input type="text" id="userName" name="userName" onChange={this.handleChange} value={this.state.userName} />
-        <br />
+      <Formik
+        onSubmit={this.handleSubmit}
+        validationSchema={this.validationSchema}
+        render={(formikProps) => {
+          return (
+            <div>
+              <h1>Login Example</h1>
 
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" onChange={this.handleChange} value={this.state.password} />
-        <p>{this.state.passwordError}</p>
-        <br />
+              <label htmlFor="userName">User Name</label>
+              <input type="text" 
+              id="userName" 
+              name="userName" 
+              onChange={formikProps.handleChange} 
+              value={formikProps.userName} />
+              <p>{formikProps.errors.userName}</p>
+              <br />
 
-        <button onClick={this.onSigninClicked}>Sign in</button>
-      </div>
+              <label htmlFor="password">Password</label>
+              <input type="password" 
+              id="password" 
+              name="password" 
+              onChange={formikProps.handleChange} 
+              value={formikProps.password} />
+              <p>{formikProps.errors.password}</p>
+              <br />
+
+              <button onClick={formikProps.submitForm} disabled={!formikProps.isValid}>Sign in</button>
+            </div>
+          )
+        }}
+      />
+      // <div className="App">
+
+      // </div>
     );
   }
 }
